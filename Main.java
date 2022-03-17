95441a2bbd634bd22f6232f439e95cd979a3d388
Ryuki Tsushida <r.tsushida.sys21@morijyobi.ac.jp>
2022-03-17 12:00:44 +0900

    Main.javaと.gitignoreの新規作成

---
+++ Main.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main{
+	public static void main (String[] args ){
+		Scanner stdIn = new Scanner(System.in);
+
+        double sintyou = stdIn.nextDouble(); 
+        double taijyu = stdIn.nextDouble(); 
+
+		double bmi = taijyu / Math.pow(sintyou, 2);
+
+		System.out.println(bmi);
+	}
+}
